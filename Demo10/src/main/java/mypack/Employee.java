a6cda822c17c9550e1e2cb707780736ed231cd8e
Gino <sunjapan@126.com>
2017-01-22 09:09:53 +0800

    @Autowired on constructor

---
+++ Demo10/src/main/java/mypack/Employee.java
@@ -0,0 +1,34 @@
+package mypack;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+	private String ename;
+	private String role;
+	@Autowired
+	private Address address;
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
